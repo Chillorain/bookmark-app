69472858ac1949127878af6c2ecd6b1d51b19156
Andrey <andrey.kalugin.34521@gmail.com>
2026-09-07 23:25:57 +0500

    add pinia, axios

---
+++ src/components/ButtonIcon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { size = 32 } = defineProps<{ size?: number }>();
+</script>
+
+<template>
+  <button class="button-icon" :style="{ width: `${size}px`, height: `${size}px` }">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--color-fg);
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+}
+.button-icon:hover {
+  background: var(--color-fg-hover);
+}
+</style>
--- src/components/ProfileAvatar.vue
+++ src/components/ProfileAvatar.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-const { name } = defineProps<{
-  name: string;
-}>();
+import { onMounted } from 'vue';
+import { useProfileStore } from '@/stores/profile.store';
+
+const store = useProfileStore();
+
+onMounted(() => {
+  store.fetchProfile();
+});
 </script>
 
 <template>
-  <div class="profile-avatar">
-    <img src="/public/avatar.png" class="profile-avatar__image" alt="Avatar" />
+  <div class="profile-avatar" v-if="store.profile">
+    <img src="/avatar.png" class="profile-avatar__image" alt="Avatar" />
     <div>
-      Привет, <strong>{{ name }}!</strong>
+      Привет, <strong>{{ store.profile.name }}!</strong>
     </div>
   </div>
 </template>
@@ -19,6 +24,7 @@ const { name } = defineProps<{
   flex-direction: column;
   gap: 24px;
   font-size: 18px;
+  margin-bottom: 40px;
 }
 .profile-avatar__image {
   width: 80px;
